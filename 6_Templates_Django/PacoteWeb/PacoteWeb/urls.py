38dba606663ffb01ea84a50f7a206d46c335a795
Jarom kepri <68934855+KepryDeSousa@users.noreply.github.com>
2023-05-07 18:59:41 +0000

    Retroceder

--- 6_Templates_Django/PacoteWeb/PacoteWeb/urls.py
+++ 6_Templates_Django/PacoteWeb/PacoteWeb/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path("Aplicacao_I/", include("Aplicacao_I.urls")),
+    path("", include("Aplicacao_I.urls")),
     path("admin/", admin.site.urls),
     
 ]
