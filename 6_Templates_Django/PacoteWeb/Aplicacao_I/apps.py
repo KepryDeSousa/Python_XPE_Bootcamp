904b8b1bc66623cd21b6170f71fb09f00f7bba75
Jarom kepri <68934855+KepryDeSousa@users.noreply.github.com>
2023-05-07 19:00:28 +0000

    Recomeçar o desenvolvimento de templates

--- 6_Templates_Django/PacoteWeb/Aplicacao_I/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AplicacaoIConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "Aplicacao_I"
--- 6_Templates_Django/PacoteWeb/Aplicacao_I/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import index
-# incluindo nossos codigos
-
-
-urlpatterns = {
-    path("",index, name="index"),
-}
